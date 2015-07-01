f064d65e1648d83c268f43e2b543e2ad7760bc89
Vlad <myair@Mys-MacBook-Air.shome>
2015-07-01 13:24:29 +0300

    first commit

---
+++ HNReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HNReader
+//
+//  Created by Vlad on 7/1/15.
+//  Copyright (c) 2015 Vlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HNReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HNReader
+//
+//  Created by Vlad on 7/1/15.
+//  Copyright (c) 2015 Vlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
